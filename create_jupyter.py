1aace7f4267553fe855bd5c916b0edf9c8c0dfdf
uniras <tkappeng@gmail.com>
2024-07-14 01:21:27 +0900

    Add files via upload

--- create_jupyter.py
+++ create_jupyter.py
@@ -2,6 +2,15 @@ import sys
 import os
 import subprocess
 
+# インストールするパッケージをリストで指定します。
+required_packages = [
+    "jupyter",
+]
+
+# インストール後にこのスクリプトを削除する場合はtrueに設定します。
+remove_script = False
+
+
 # インストールスクリプトが存在するディレクトリ
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -12,12 +21,10 @@ os.chdir(script_dir)
 venv_dir = os.path.join(script_dir, ".venv")
 
 # ステップ1: .venvディレクトリを確認して存在する場合は中止
-files = os.listdir(script_dir)
+dirs = os.listdir(script_dir)
 
-if ".venv" in files:
-    print(
-        f'"{script_dir}" ディレクトリにはすでに仮想環境ディレクトリがあります。仮想環境の作成を中止します。'
-    )
+if ".venv" in dirs:
+    print(f""""{script_dir}" ディレクトリにはすでに仮想環境ディレクトリがあります。仮想環境の作成を中止します。""")
     input("続行するには何かキーを押してください...")
     sys.exit(1)
 
@@ -37,18 +44,18 @@ else:
 pip_path = os.path.join(venv_dir, binname, "pip")
 
 # ステップ5: 必要なパッケージのインストール
-required_packages = ["jupyter"]
-
 print(f'必要なパッケージを "{venv_dir}" 環境にインストールしています...')
 subprocess.run([pip_path, "install"] + required_packages)
 
 # ステップ6: 自身のスクリプトを削除
-print("インストールスクリプトを削除しています...")
-os.remove(__file__)
-print(f'ファイル "{__file__}" を削除しました。')
+if remove_script:
+    print("インストールスクリプトを削除しています...")
+    os.remove(__file__)
+    print(f'ファイル "{__file__}" を削除しました。')
 
 # 終了メッセージの出力と待機
 print("Jupyter環境のインストールが完了しました。")
-print('VSCodeでJupyterノートブックを開始するには、VSCodeを起動して、F1を押して、"Jupyter: Create New Blank Notebook"を選択します。')
-print('VSCodeのJupyterで仮想環境を有効にするには、"Select Kernel"をクリックし、"Python Environment"を選択した後、作成した".venv"ディレクトリを選択します。')
+print("""VSCodeでJupyterノートブックを開始するには、VSCodeを起動して、F1を押して、"Jupyter: Create New Blank Notebook"を選択します。""")
+print("""VSCodeのJupyterで仮想環境を有効にするには、"Select Kernel"をクリックし、"Python Environment"を選択した後、作成した".venv"ディレクトリを選択します。""")
+print("""ターミナルで仮想環境のPythonを有効にするには、".venv"ディレクトリに移動して、"Scripts"または"bin"ディレクトリにある"Activate"スクリプトを実行します。""")
 input("続行するには何かキーを押してください...")
